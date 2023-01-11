d88dea9ab049b77a811a5b8baa8de2c80bc087f6
oscarmartinez6 <121180162+oscarmartinez6@users.noreply.github.com>
2023-01-11 12:15:21 -0600

    Update config.php
    Se creó la conexión con la base de datos.

--- config/config.php
+++ config/config.php
@@ -1,6 +1,16 @@
 <?php
 
-$host = "localhost";
-$dbname = "jobboard";
-$user = "root";
-$pass = "";
+try {
+    //Conexión a la BD.
+    $host = "localhost";
+    $dbname = "jobboard";
+    $user = "root";
+    $pass = "";
+
+    $conn = new PDO("mysql:host = $host; dbname = $dbname", $user, $pass);
+    //Linea sacada de la documentación de php. Solo se cambió la variable $conn
+    $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch(PDOException $e) {
+    echo $e -> getMessage();
+}
