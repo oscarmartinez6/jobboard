820ce836e475b729f99efd7249e4a6ff11179c8d
oscarmartinez6 <121180162+oscarmartinez6@users.noreply.github.com>
2023-01-13 10:25:35 -0600

    Notas y comentarios.
    Se agregaron nuevos comentarios y algunos tips para tener en cuenta en el proyecto.

--- config/config.php
+++ config/config.php
@@ -7,7 +7,8 @@ try {
     $user = "root";
     $pass = "";
 
-    $conn = new PDO("mysql:host = $host; dbname = $dbname", $user, $pass);
+    //NO SE DEBEN DEJAR ESPACIOS EN BLANCO
+    $conn = new PDO("mysql:host=$host;dbname=$dbname",$user,$pass);
     //Linea sacada de la documentación de php. Solo se cambió la variable $conn
     $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
--- includes/header.php
+++ includes/header.php
@@ -21,12 +21,12 @@
             <link rel="stylesheet" href="../css/style.css">    
         </head>
         <body id="top">
-            <div id="overlayer"></div>
+            <!--<div id="overlayer"></div>
             <div class="loader">
                 <div class="spinner-border text-primary" role="status">
                     <span class="sr-only">Loading...</span>
                 </div>
-            </div>
+            </div> -->
     
             <div class="site-wrap">
                 <div class="site-mobile-menu site-navbar-target">
